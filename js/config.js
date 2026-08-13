23891916ce9dca50b303700b65bfc010221060b7
Harinto José Rodríguez Cruz <djhjrc96@gmail.com>
2026-08-12 19:03:18 -0600

    firebase

--- js/config.js
+++ js/config.js
@@ -14,11 +14,12 @@ export const STORE_CONFIG = {
 
 // Reemplaza estos valores por los de tu proyecto Firebase cuando quieras separar
 // este sitio del proyecto original. Las API keys de Firebase Web no son secretas.
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAPUPxZNuJfX3KtiI1p73AAfDgNqVo8a-g",
-  authDomain: "tienda-angeles-beauty.firebaseapp.com",
-  projectId: "tienda-angeles-beauty",
-  storageBucket: "tienda-angeles-beauty.firebasestorage.app",
-  messagingSenderId: "913588395045",
-  appId: "1:913588395045:web:ce50c2f7a296c85ecff3cc"
+export const firebaseConfig = {
+  apiKey: "AIzaSyD99PK3D8ZBqqkWqRl8x_nXCHoOYV1UKvI",
+  authDomain: "articulos-ferreteros-eh.firebaseapp.com",
+  databaseURL: "https://articulos-ferreteros-eh-default-rtdb.firebaseio.com",
+  projectId: "articulos-ferreteros-eh",
+  storageBucket: "articulos-ferreteros-eh.firebasestorage.app",
+  messagingSenderId: "774436339108",
+  appId: "1:774436339108:web:8a83f8289037fe6c5d5531"
 };
